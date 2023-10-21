c1bc24635358ffa7a9d2cf79a840866f724b45c6
MSI75 <sisaif56@gmail.com>
2023-10-21 12:56:22 +0600

    Different Divisors

---
+++ NumberTheory/Different_Divisors.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+bool isPrime(ll n)
+{
+    if (n == 1)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void TEST_CASES()
+{
+    ll d;
+    cin >> d;
+    ll x, y;
+    x = d + 1;
+    while (!isPrime(x))
+        x++;
+    y = d + x;
+    while (!isPrime(y))
+        y++;
+    cout << (x * y) << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
