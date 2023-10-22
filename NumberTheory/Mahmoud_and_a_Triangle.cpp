da46a77c425fdd567c2a0460dcee4ad3cfa3c2b3
MSI75 <sisaif56@gmail.com>
2023-10-22 09:38:56 +0600

    Mahmoud and a Triangle

---
+++ NumberTheory/Mahmoud_and_a_Triangle.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (a[i] + a[i + 1] > a[i + 2])
+        {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
