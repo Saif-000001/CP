57ffe04e6b44c1b343f2ed1b1365fcb7699800e0
MSI75 <sisaif56@gmail.com>
2023-10-27 16:43:42 +0600

    Modex

---
+++ Topics/BinaryExpression/MODEX.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#ifdef LOKAL
+#include "DEBUG_TEMPLATE.h"
+#else
+#define HERE
+#define debug(args...)
+#endif
+const int MOD = 1000000007;
+const int N = 2e5 + 5;
+typedef pair<int, int> pii;
+
+void TEST_CASES()
+{
+    ll x, y, n;
+    cin >> x >> y >> n;
+
+    ll res = 1;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res * x) % n;
+        x = (x * x) % n;
+        y >>= 1;
+    }
+    cout << res << "\n";
+}
+
+int32_t main()
+{
+#ifndef LOKAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        TEST_CASES();
+    }
+    return 0;
+}
